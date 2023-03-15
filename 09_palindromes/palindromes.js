43d8b697892e1e3fdabc9fcf0d852c04a61b8b19
Ryan <r.alcoran@gmail.com>
2023-03-15 11:32:53 -0400

    Added solution for 09_palindromes and 10_fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	let clean = '';
+	for (var i = 0; i < string.length; i++) {
+		if (string[i].toLowerCase() != string[i].toUpperCase()){
+			clean += string[i];
+		}
+	}
+	if(clean.toLowerCase().split('').reverse().join('') === clean.toLowerCase()){
+		return true;
+	}
+	else{
+		return false
+	}
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+	if (num < 0){
+		return "OOPS";
+	}
+	let prev = 0;
+	let curr = 1;
+		for (var i = 1; i < num; i++) {
+		let temp = curr;
+		curr = prev + curr;
+		prev = temp;
+	}
+	return curr;
 };
 
 // Do not edit below this line
